031c072722a1c1b4e908e86fc1a1601c35a48b83
jorgedediosb <jorgededios@icloud.com>
2024-07-10 20:13:14 +0200

    update SQL files

---
+++ MySQL/2. resolucion_consultas.sql
@@ -0,0 +1,89 @@
+-- Consultas
+
+-- NIVEL 1
+-- Listar todos los alumnos inscriptos en el curso de "Guitarra Intermedio"
+--Solución 1 (con JOIN explícito)
+SELECT a.nombre, a.apellido
+FROM alumnos a
+JOIN inscripciones i ON a.id_alumno = i.id_alumno
+JOIN cursos c ON i.id_curso = c.id_curso
+WHERE c.nombre = "Guitarra Intermedio";
+
+-- Solución 2 (sin JOIN - JOIN implícito)
+SELECT a.nombre, a.apellido
+FROM alumnos a, inscripciones i, cursos c
+WHERE a.id_alumno = i.id_alumno AND 
+		i.id_curso= c.id_curso AND
+        c.nombre = "Guitarra Intermedio";
+
+-- Mostrar todas las inscripciones realizadas después del 1 de enero de 2024
+SELECT *
+FROM inscripciones
+WHERE fecha_insc > '2024-01-01';
+
+-- Contar la cantidad de inscripciones abonadas
+SELECT COUNT(*)
+FROM inscripciones
+WHERE abono_insc = TRUE;
+
+-- Listar los cursos de nivel avanzado
+SELECT *
+FROM cursos
+WHERE nivel_habilidad = "Avanzado";
+----
+
+
+-- NIVEL 2
+-- Listar los nombres y apellidos de los alumnos junto con los nombres de los cursos a los que están inscritos.
+SELECT a.nombre, a.apellido, c.nombre
+FROM alumnos a
+JOIN inscripciones i ON a.id_alumno = i.id_alumno
+JOIN cursos c ON i.id_curso = c.id_curso;
+
+-- Obtener el nombre y apellido de los alumnos que están inscritos en más de un curso.
+    -- 1º Traer los alumnos
+    -- 2º Contar la cantidad de cursos en la que está cada alumno inscripto
+    -- 3º Filtrar los que estén en más de 1
+
+SELECT nombre, apellido
+FROM alumnos
+WHERE id_alumno IN (
+	SELECT id_alumno
+    FROM inscripciones
+    GROUP BY id_alumno
+    HAVING COUNT(id_curso)>1   
+);
+
+--Mostrar el nombre de cada curso y la cantidad de alumnos inscritos en cada uno.
+SELECT c.nombre, COUNT(i.id_alumno)
+FROM cursos c
+JOIN inscripciones i ON c.id_curso = i.id_curso
+GROUP BY c.nombre;
+
+
+-- NIVEL 3
+-- Listar los alumnos que no han abonado la inscripción.
+SELECT a.nombre, a.apellido
+FROM alumnos a, inscripciones i
+WHERE a.id_alumno= i.id_alumno 
+AND i.abono_insc = FALSE;
+
+SELECT a.nombre, a.apellido
+FROM alumnos a
+JOIN inscripciones i ON a.id_alumno = i.id_alumno
+WHERE i.abono_insc = FALSE;
+
+-- Obtener los nombres de los cursos que tienen al menos un alumno mayor de 20 años inscrito.
+SELECT DISTINCT c.nombre
+FROM cursos c
+JOIN inscripciones i ON c.id_curso = i.id_curso
+JOIN alumnos a ON i.id_alumno = a.id_alumno
+WHERE DATEDIFF (CURDATE(), a.fecha_nac) / 365 > 20;
+
+-- Listar el nombre y apellido de los alumnos junto con los nombres de los cursos en los que están inscritos,
+-- pero solo para aquellos que se inscribieron en 2024.
+SELECT a.nombre, a.apellido, c.nombre
+FROM alumnos a
+JOIN inscripciones i ON a.id_alumno= i.id_alumno
+JOIN cursos c ON i.id_curso = c.id_curso
+WHERE YEAR(i.fecha_insc) = 2024;
--- MySQL/README.md
+++ MySQL/README.md
@@ -1,5 +1,7 @@
 MANUAL DE OPERACIÓN
 
+PROYECTO ESCUELA DE MÚSICA (https://www.youtube.com/watch?v=l3KISVZJsVo)
+
 - Abrir app XAMPP
 - En pestaña 'Manage Servers' ejecutar 'Apache Web Server' y 'MySQL Database'
 - Abrir en navegador (Firefox): http://localhost:8080/dashboard/ + PHPAdmin o directamente: http://localhost:8080/phpmyadmin/
